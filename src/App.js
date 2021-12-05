2fdc01f90eb5d90b8d33a65eb8fc1ccdaaea7de3
Dustin <dustinibrao@gmail.com>
2021-12-04 22:19:09 -1000

    added the css to make the notepads look like notpads

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-
-import './App.css';
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import NotesList from "./components/NotesList";
 
 function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+	const [notes, setNotes] = useState([
+		{
+			id: nanoid(),
+			text: "testing text one",
+			date: "dec 4 2021",
+		},
+		{
+			id: nanoid(),
+			text: "testing text two",
+			date: "dec 5 2021",
+		},
+		{
+			id: nanoid(),
+			text: "testing text three",
+			date: "dec 6 2021",
+		},
+	]);
+	return (
+		<div className="container">
+			<NotesList notes={notes} />
+		</div>
+	);
 }
 
 export default App;
